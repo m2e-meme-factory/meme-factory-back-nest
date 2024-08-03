47e135c7afc221d332562f592bef03d112bcb5e3
K0rzet <ehot2004@gmail.com>
2024-08-03 20:17:47 +0300

    files upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,13 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ProjectModule } from './project/project.module';
 import { AuthModule } from './auth/auth.module';
+import { FileService } from './file/file.service';
+import { FileController } from './file/file.controller';
+import { FileModule } from './file/file.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, ProjectModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, UserModule, ProjectModule, AuthModule, FileModule],
+  controllers: [AppController, FileController],
+  providers: [AppService, FileService],
 })
 export class AppModule {}
