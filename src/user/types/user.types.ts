6b21a3f969af4ab61467f005b1415dfaed8e73b6
K0rzet <ehot2004@gmail.com>
2024-08-17 15:10:21 +0300

    fixed user interface field balance from number to Decimal

--- src/user/types/user.types.ts
+++ src/user/types/user.types.ts
@@ -1,10 +1,11 @@
 import { UserRole } from "@prisma/client"
+import { Decimal } from "@prisma/client/runtime/library"
 
 export interface IUser {
 	id: number
 	telegramId: string
 	username: string
-	balance: number
+	balance: Decimal
 	isBaned: boolean
 	isVerified: boolean
 	createdAt: Date
