6d324f692ecd8e4a8e61c01ee683a54878b88c70
K0rzet <ehot2004@gmail.com>
2024-08-17 14:26:50 +0300

    fixed app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ProjectModule } from './project/project.module';
@@ -14,7 +12,7 @@ import { EventModule } from './event/event.module';
 
 @Module({
   imports: [PrismaModule, UserModule, ProjectModule, AuthModule, FileModule, TelegramModule, TransactionModule, EventModule],
-  controllers: [AppController, FileController],
-  providers: [AppService, FileService],
+  controllers: [FileController],
+  providers: [FileService],
 })
 export class AppModule {}
