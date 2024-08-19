26601997eb8890b36eea77e996fde36ca95879e8
K0rzet <ehot2004@gmail.com>
2024-08-19 17:49:56 +0300

    maybe fixed refs bug, should be tested

--- src/telegram/telegram.update.ts
+++ src/telegram/telegram.update.ts
@@ -2,23 +2,23 @@
 import { Update, Ctx, Start, InjectBot } from 'nestjs-telegraf'
 import { PublicRoute } from 'src/auth/decorators/public-route.decorator'
 import { UserService } from 'src/user/user.service'
-import { Context, Markup, Telegraf } from 'telegraf'
+import { Context, Telegraf } from 'telegraf'
 
-const ORIGIN_URL = "https://api.digital-boost.site/uploads/projects";
+const ORIGIN_URL = 'https://api.digital-boost.site/uploads/projects'
 
 @Update()
 export class TelegramUpdate {
 	constructor(
 		@InjectBot() private readonly bot: Telegraf<Context>,
 		private readonly userService: UserService
-	) { }
+	) {}
 
 	@Start()
 	@PublicRoute()
 	async startCommand(@Ctx() ctx: Context) {
 		const messageText = ctx.text
 		const inviterRefCode = messageText.split(' ')[1]
-		await this.userService.findOrCreateUser(
+		const user = await this.userService.findOrCreateUser(
 			ctx.from.id,
 			ctx.from.username,
 			inviterRefCode
@@ -31,14 +31,14 @@ export class TelegramUpdate {
 					[
 						{
 							text: 'Открыть приложение',
-							web_app: { url: webAppUrl + "/projects" }
+							web_app: { url: webAppUrl + '/projects' }
 						}
 					]
 				]
 			}
 		})
 
-		if (inviterRefCode) {
+		if (inviterRefCode && !user.inviterRefCode) {
 			const inviter = await this.userService.getUserByRefCode(inviterRefCode)
 
 			if (inviter) {
@@ -49,18 +49,29 @@ export class TelegramUpdate {
 			}
 		}
 	}
-	async sendFilesToUser(telegramId: string, files: string[], projectTitle: string = undefined): Promise<void> {
-		const message = files.map(
-			fileName => `- <a href="${ORIGIN_URL}/${fileName}">${fileName.substring(37)}</a>`
-		).join("\n")
-		console.log(message);
-		await this.bot.telegram.sendMessage(telegramId, `
-Download attachments for project <b>${projectTitle || ""}</b> with this links:\n
+	async sendFilesToUser(
+		telegramId: string,
+		files: string[],
+		projectTitle: string = undefined
+	): Promise<void> {
+		const message = files
+			.map(
+				fileName =>
+					`- <a href="${ORIGIN_URL}/${fileName}">${fileName.substring(37)}</a>`
+			)
+			.join('\n')
+		await this.bot.telegram.sendMessage(
+			telegramId,
+			`
+Download attachments for project <b>${projectTitle || ''}</b> with this links:\n
 ${message}
-`, { parse_mode: "HTML",
-	link_preview_options: {
-		is_disabled: true
-	}
- })
+`,
+			{
+				parse_mode: 'HTML',
+				link_preview_options: {
+					is_disabled: true
+				}
+			}
+		)
 	}
 }
