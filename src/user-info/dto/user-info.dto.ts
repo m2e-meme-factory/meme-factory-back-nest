8870f490e2daa01740c7cb43256d2a4d6a37b77b
K0rzet <ehot2004@gmail.com>
2024-08-29 14:30:38 +0300

    updated userinfo dto for  swagger for ton connect

--- src/user-info/dto/user-info.dto.ts
+++ src/user-info/dto/user-info.dto.ts
@@ -4,17 +4,17 @@ import { IsOptional, IsString } from "class-validator"
 export class UpdateUserInfoDto {
 	@IsString()
 	@IsOptional()
-  @ApiProperty()
+  @ApiProperty({required: false})
 	name?: string
-  @ApiProperty()
+  @ApiProperty({required: false})
 	@IsString()
 	@IsOptional()
 	phoneNumber?: string
-  @ApiProperty()
+  @ApiProperty({required: false})
 	@IsString()
 	@IsOptional()
 	email?: string
-  @ApiProperty()
+  @ApiProperty({required: false})
 	@IsString()
 	@IsOptional()
 	tonWalletAddress?: string
