2a09fdf83ac4647cb170b326fb99b99ce1ed4343
K0rzet <ehot2004@gmail.com>
2024-08-29 15:26:39 +0300

    try to fix download files #1

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,23 +1,24 @@
-import { Module } from '@nestjs/common';
-import { FileController } from './file.controller';
-import { FileService } from './file.service';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { path } from 'app-root-path';
+import { Module } from '@nestjs/common'
+import { FileController } from './file.controller'
+import { FileService } from './file.service'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { path } from 'app-root-path'
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: `${path}/uploads`,
-      serveRoot: '/uploads',
-      serveStaticOptions: {
-    	index: false,
-        setHeaders: (res) => {
-          res.setHeader('Content-Disposition', 'attachment');
-        },
-      },
-    }),
-  ],
-  controllers: [FileController],
-  providers: [FileService]
+	imports: [
+		ServeStaticModule.forRoot({
+			rootPath: `${path}/uploads`,
+			serveRoot: '/uploads',
+			serveStaticOptions: {
+				index: false,
+				setHeaders: res => {
+					res.setHeader('Content-Disposition', 'attachment')
+					res.setHeader('Content-Type', 'application/octet-stream')
+				}
+			}
+		})
+	],
+	controllers: [FileController],
+	providers: [FileService]
 })
 export class FileModule {}
