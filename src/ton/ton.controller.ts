560aded7c84ffdf0297a93f2374e595741138141
K0rzet <ehot2004@gmail.com>
2024-08-29 14:16:35 +0300

    swagger for ton connect

--- src/ton/ton.controller.ts
+++ src/ton/ton.controller.ts
@@ -1,16 +1,30 @@
 import { Body, Controller, Post, Req } from '@nestjs/common'
 import { TonService } from './ton.service'
 import { UpdateUserInfoDto } from 'src/user-info/dto/user-info.dto'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 
+@ApiTags('ton')
 @Controller('ton')
 export class TonController {
 	constructor(private readonly tonService: TonService) {}
 
 	@Post('connect')
-	async connect(@Body() updateUserInfoDto: UpdateUserInfoDto, @Req() req: Request) {
-    const user = req["user"]
-		const updatedUserInfo = await this.tonService.connectTonAddress(user.id, updateUserInfoDto)
+	@ApiOperation({ summary: 'Connect TON Wallet Address to User Account' })
+	@ApiResponse({
+		status: 200,
+		description: 'TON Wallet Address successfully connected.'
+	})
+	@ApiResponse({ status: 500, description: 'Internal Server Error.' })
+	async connect(
+		@Body() updateUserInfoDto: UpdateUserInfoDto,
+		@Req() req: Request
+	) {
+		const user = req['user']
+		const updatedUserInfo = await this.tonService.connectTonAddress(
+			user.id,
+			updateUserInfoDto
+		)
 
-    return updatedUserInfo
+		return updatedUserInfo
 	}
 }
