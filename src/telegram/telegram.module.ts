adc4078e3cc6941722192683b1c81403d93d8b0b
K0rzet <ehot2004@gmail.com>
2024-09-02 18:23:29 +0300

    1234

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -1,33 +1,34 @@
-// telegram.module.ts
-import { Module } from '@nestjs/common'
-import { TelegrafModule } from 'nestjs-telegraf'
-import { TelegramUpdate } from './telegram.update'
-import { MessageSequenceScene } from './message-sequence.scene'
-import { UserService } from 'src/user/user.service'
-import { Agent } from 'https'
+import { Module } from '@nestjs/common';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { TelegramUpdate } from './telegram.update';
+import { MessageSequenceScene } from './message-sequence.scene';
+import { UserService } from 'src/user/user.service';
+import { Agent } from 'https';
+import { session } from 'telegraf';
 
 @Module({
-	imports: [
-		TelegrafModule.forRoot({
-			token: process.env.BOT_TOKEN,
-			options: {
-				telegram: {
-					agent: new Agent({ keepAlive: false })
-				}
-			}
-		})
-	],
-	providers: [
-		TelegramUpdate,
-		MessageSequenceScene,
-		UserService,
-		{
-			provide: 'TELEGRAM_SCENES',
-			useFactory: (messageSequenceScene: MessageSequenceScene) => {
-				return [messageSequenceScene.scene]
-			},
-			inject: [MessageSequenceScene]
-		}
-	]
+  imports: [
+    TelegrafModule.forRoot({
+      token: process.env.BOT_TOKEN,
+      middlewares: [session()],
+      options: {
+        telegram: {
+          agent: new Agent({ keepAlive: false })
+        }
+      }
+    })
+  ],
+  providers: [
+    TelegramUpdate,
+    MessageSequenceScene,
+    UserService,
+    {
+      provide: 'TELEGRAM_SCENES',
+      useFactory: (messageSequenceScene: MessageSequenceScene) => {
+        return [messageSequenceScene.scene];
+      },
+      inject: [MessageSequenceScene],
+    },
+  ],
 })
 export class TelegramModule {}
